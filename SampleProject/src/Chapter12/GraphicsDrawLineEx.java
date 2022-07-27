17bd7c847a7973dc3e1e7c666ad7a329a05178c6
오세영 <@192.168.25.16>
2022-07-27 17:15:10 +0900

    자바 12장 그래픽

---
+++ SampleProject/src/Chapter12/GraphicsDrawLineEx.java
@@ -0,0 +1,33 @@
+package Chapter12;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class GraphicsDrawLineEx extends JFrame {
+	private MyPanel panel = new MyPanel();
+	
+	public GraphicsDrawLineEx() {
+		setTitle("drawLine 사용 예제");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setContentPane(panel);
+		
+		setSize(200, 170);
+		setVisible(true);
+	}
+	
+	class MyPanel extends JPanel {
+		public void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			g.setColor(Color.RED);//빨간색 선택
+			g.drawLine(20, 20, 100, 100);//선 그리기
+		}
+	}
+
+	public static void main(String[] args) {
+		new GraphicsDrawLineEx();
+	}
+
+}
